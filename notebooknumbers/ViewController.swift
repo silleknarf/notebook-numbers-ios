c7d1f511438f7dec391e3a9bfa2ca0fae02bdd3e
silleknarf@gmail.com <silleknarf@gmail.com>
2017-07-22 17:41:52 +0100

    Added iOS swift code

--- notebooknumbers/ViewController.swift
+++ notebooknumbers/ViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var webView: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Load the notebook numbers html
+        let localfilePath = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory:"notebook-numbers");
+        let myRequest = URLRequest(url: localfilePath!);
+        webView.loadRequest(myRequest);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        // Make full screen
+        webView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height);
+    }
 }
 
