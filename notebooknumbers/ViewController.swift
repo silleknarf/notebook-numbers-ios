7f86f91b352b0cf414cb407fc7ec47b193483d5f
silleknarf@gmail.com <silleknarf@gmail.com>
2017-08-13 20:00:03 +0100

    Added leaderboards code and JavaScript bridge pod

--- notebooknumbers/ViewController.swift
+++ notebooknumbers/ViewController.swift
@@ -7,18 +7,98 @@
 //
 
 import UIKit
+import GameKit
+import SwiftJavaScriptBridge
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GKGameCenterControllerDelegate {
     @IBOutlet weak var webView: UIWebView!
+    
+    /* Variables */
+    var gcEnabled = Bool() // Check if the user has Game Center enabled
+    var gcDefaultLeaderBoard = String() // Check the default leaderboardID
+    
+    var score = 0
+    
+    // IMPORTANT: replace the red string below with your own Leaderboard ID (the one you've set in iTunes Connect)
+    let LEADERBOARD_ID = "com.silleknarf.notebooknumbers.highscores"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // Call the GC authentication controller
+        authenticateLocalPlayer();
+        
+        let bridge = SwiftJavaScriptBridge.bridge(webView, defaultHandler: { data, responseCallback in
+            print("Swift received message from JS: \(data)")
+            responseCallback("Swift already got your msg, thanks")
+        })
+        bridge.
+        
         // Load the notebook numbers html
         let localfilePath = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "notebook-numbers");
         let myRequest = URLRequest(url: localfilePath!);
         webView.loadRequest(myRequest);
+    }
+    
+    // MARK: - AUTHENTICATE LOCAL PLAYER
+    func authenticateLocalPlayer() {
+        let localPlayer: GKLocalPlayer = GKLocalPlayer.localPlayer()
+        
+        localPlayer.authenticateHandler = {(ViewController, error) -> Void in
+            if((ViewController) != nil) {
+                // 1. Show login if player is not logged in
+                self.present(ViewController!, animated: true, completion: nil)
+            } else if (localPlayer.isAuthenticated) {
+                // 2. Player is already authenticated & logged in, load game center
+                self.gcEnabled = true
+                
+                // Get the default leaderboard ID
+                localPlayer.loadDefaultLeaderboardIdentifier(completionHandler: { (leaderboardIdentifer, error) in
+                    if error != nil {
+                        print(error!)
+                    } else {
+                        self.gcDefaultLeaderBoard = leaderboardIdentifer!
+                    }
+                })
+            } else {
+                // 3. Game center is not enabled on the users device
+                self.gcEnabled = false
+                print("Local player could not be authenticated!")
+                print(error!)
+            }
+        }
+    }
+    
+    // MARK: - ADD 10 POINTS TO THE SCORE AND SUBMIT THE UPDATED SCORE TO GAME CENTER
+    @IBAction func addScoreAndSubmitToGC(_ sender: AnyObject) {
+        // Add 10 points to current score
+        score += 10
+        
+        // Submit score to GC leaderboard
+        let bestScoreInt = GKScore(leaderboardIdentifier: LEADERBOARD_ID)
+        bestScoreInt.value = Int64(score)
+        GKScore.report([bestScoreInt]) { (error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            } else {
+                print("Best Score submitted to your Leaderboard!")
+            }
+        }
+    }
+    
+    // MARK: - OPEN GAME CENTER LEADERBOARD
+    @IBAction func checkGCLeaderboard(_ sender: AnyObject) {
+        let gcVC = GKGameCenterViewController()
+        gcVC.gameCenterDelegate = self
+        gcVC.viewState = .leaderboards
+        gcVC.leaderboardIdentifier = LEADERBOARD_ID
+        present(gcVC, animated: true, completion: nil)
+    }
+    
+    // Delegate to dismiss the GC controller
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismiss(animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
