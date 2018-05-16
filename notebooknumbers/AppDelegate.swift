ccb5b49eb3b095fb2d3ad1d31a8fa910220761c4
Frank Ellis <887612+silleknarf@users.noreply.github.com>
2018-05-16 19:32:09 +0100

    Fixed formatting

--- notebooknumbers/AppDelegate.swift
+++ notebooknumbers/AppDelegate.swift
@@ -13,9 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
         // Set the user agent
         let customUserAgent = "notebook-numbers-ios";
@@ -48,7 +46,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
